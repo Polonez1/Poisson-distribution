157d10e7bf072ef84143b83dcc69482f3df9aff1
Polonez1 <64654372+Polonez1@users.noreply.github.com>
2022-03-14 18:25:29 +0200

    Get results

---
+++ result.py
@@ -0,0 +1,6 @@
+class r:
+    def result(df):
+        res = df[['home_team_name','away_team_name','Game Week','home_team_goal_count','away_team_goal_count','total_goal_count']]
+        return res
+ 
+
