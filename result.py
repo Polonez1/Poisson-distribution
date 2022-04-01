31410064f77fad06e1a453daa82d9f857d65d287
Polonez1 <64654372+Polonez1@users.noreply.github.com>
2022-04-01 14:58:30 +0300

    Delete result.py

--- result.py
+++
@@ -1,6 +0,0 @@
-class r:
-    def result(df):
-        res = df[['home_team_name','away_team_name','Game Week','home_team_goal_count','away_team_goal_count','total_goal_count']]
-        return res
- 
-
